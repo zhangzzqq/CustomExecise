e0a8b61f0de629ed30dff4d8a12d667f6072c58c
zhnagqihappyzq <zhangqihappyzq@163.com>
2016-12-29 17:47:07 +0800

    提交

--- popwindowtest/src/main/java/com/example/zq/popwindowtest/MainActivity.java
+++ popwindowtest/src/main/java/com/example/zq/popwindowtest/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.zq.popwindowtest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tv;
     private LinearLayout ll;
+    private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.tv);
         ll = (LinearLayout) findViewById(R.id.activity_main);
+        mBtn = (Button) findViewById(R.id.btn1);
+        mBtn.setOnClickListener(this);
         initPopWindow1();
     }
 
     private void initPopWindow1() {
-        
 //        PopUtils pop = new PopUtils(this);
 //        pop.initPop(tv);
 //        pop.showPop(null,ll);
@@ -32,14 +35,29 @@ public class MainActivity extends AppCompatActivity {
          * 3.窗口焦点改变
          */
     }
-
     //当activity获得焦点之后，activity是加载完毕的了
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if(hasFocus){
-            MorePopWindow popWindow = new MorePopWindow(this);
-            popWindow.showPopupWindow((View) tv.getParent());   
+//            MorePopWindow popWindow = new MorePopWindow(this);
+//            popWindow.showPopupWindow((View) tv.getParent());   
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        
+        switch (view.getId()){
+            
+            case R.id.btn1 :
+
+                MorePopWindow popWindow = new MorePopWindow(this);
+                popWindow.showPopupWindow((View) mBtn.getParent());
+                break;
+            
+            
+            
         }
     }
 }
