7bd8f9495c53d8585c1c586a0abb31428412e4ab
zhnagqihappyzq <zhangqihappyzq@163.com>
2016-12-30 17:19:59 +0800

    提交

---
+++ takephototest/src/main/java/com/example/zq/takephototest/Album.java
@@ -0,0 +1,183 @@
+package com.example.zq.takephototest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Album extends Activity  implements View.OnClickListener{
+	
+	private static int REQCODE1=1;
+	private static int REQCODE2=2;
+	private static int REQCODE3=3;
+	private File tmDir;
+	private ImageView image;
+	private Bitmap bitmap;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main2);
+		
+		initData();
+	}
+
+	private void initData() {
+		
+		Button btnCameral = (Button) findViewById(R.id.btn_camera);
+		Button btnGallery = (Button) findViewById(R.id.btn_gallery);
+		image = (ImageView) findViewById(R.id.iv);
+		btnCameral.setOnClickListener(this);
+		btnGallery.setOnClickListener(this);
+	
+		boolean sdCard = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+		if(sdCard){
+			String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"temp.png";
+			tmDir = new File(filePath);
+			//转换为file对象 如果路径不存在，则创建
+			if(!tmDir.exists()){
+				tmDir.mkdirs();
+			}
+		}
+//		File tmpDir = new File(Environment.getExternalStorageDirectory() + "/com.jikexueyuan.avater");
+//		//如果路径不存在，则创建
+//		if(!tmpDir.exists())
+//		{
+//			tmpDir.mkdir();
+//		}
+//		//保存在这个文件夹下
+//		File file_img = new File(tmpDir.getAbsolutePath() + "avater.png");
+		
+		//回收bitmap
+		destroyBitmap();
+		
+	}
+
+
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()){
+			
+			case R.id.btn_camera:
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivityForResult(intent,REQCODE1);
+				break;
+			case R.id.btn_gallery:
+				Intent intent2 = new Intent(Intent.ACTION_GET_CONTENT);
+				intent2.setType("image/*");
+				startActivityForResult(intent2,REQCODE2);
+				
+				break;
+		}
+	}
+
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if(resultCode==RESULT_OK)
+		{
+			if(requestCode==REQCODE1){
+				Bundle bundle = data.getExtras();
+				Bitmap bitmap = (Bitmap) bundle.get("data");
+				//进行裁减处理
+				Uri uri  = saveBitmap(bitmap);
+				startImageZoom(uri);
+			}else if(requestCode==REQCODE2){
+				Uri uri = data.getData();
+				Uri fileUri = convertUri(uri);
+				startImageZoom(fileUri);
+			}else if(requestCode==REQCODE3){
+				Bundle bundle = data.getExtras();
+				bitmap =  bundle.getParcelable("data");
+				image.setImageBitmap(bitmap);
+			}
+		}
+	}
+	//拍照
+	private Uri saveBitmap(Bitmap mBitmap) {
+
+		try {
+			FileOutputStream fos = new FileOutputStream(tmDir);
+			//把图像写入流中
+			mBitmap.compress(Bitmap.CompressFormat.PNG,100,fos);
+			fos.flush();
+			fos.close();
+			//把file文件转换为uri 
+			return Uri.fromFile(tmDir);
+			
+		}  catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	//相册
+	private Uri convertUri(Uri uri) {
+
+		InputStream is = null;
+
+		try {
+			is = getContentResolver().openInputStream(uri);
+			Bitmap bitmap = BitmapFactory.decodeStream(is);
+			is.close();
+			saveBitmap(bitmap);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	
+	//照片裁减
+	private void startImageZoom(Uri uri) {
+		Intent intent = new Intent ("com.android.camera.action.CROP");
+		//设置uri和图像类型
+		intent.setDataAndType(uri,"image/*");
+		intent.putExtra("crop","true");
+		//缩放比例
+		intent.putExtra("aspectX",1);
+		intent.putExtra("aspectY",1);
+		//裁减图片的宽高
+		intent.putExtra("outputX",150);
+		intent.putExtra("outputY",150);
+		//返回数据
+		intent.putExtra("return-data",true);
+		startActivityForResult(intent,REQCODE3);
+	
+	}
+
+	private void destroyBitmap() {
+		
+		if(bitmap!=null&&!bitmap.isRecycled()){
+			
+			bitmap.recycle();
+			bitmap=null;
+		}
+		
+	}
+	
+	
+	
+}
---
+++ takephototest/src/main/java/com/example/zq/takephototest/TakePhoto.java
@@ -0,0 +1,122 @@
+package com.example.zq.takephototest;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * 注意权限，大于22会出现异常，因为没有获得写入权限或者拍照权限
+ */
+public class TakePhoto extends AppCompatActivity {
+    private static final int REQ_1 = 1;
+    private static final int REQ_2 = 2;
+    private static final int REQ_3 = 3;
+    private ImageView imageView ;
+    private String mFilePath;
+    FileInputStream fis;
+    Bitmap bitmap=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+    }
+
+    private void initData() {
+        imageView = (ImageView) findViewById(R.id.iv);
+        //获取路径 
+        boolean sdCardExist = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+        if(sdCardExist){
+            mFilePath = Environment.getExternalStorageDirectory().getPath()+File.separator+"temp.png";
+        }
+        destroyBitmap(); 
+    }
+    
+
+    //调用系统的camera ,缩略图
+        public void startCamera1(View view){
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent,REQ_1);
+        }
+            //调用系统相机，保存原图
+            public void startCamera2(View view){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //file转换为uri
+                Uri bitmapUri = Uri.fromFile(new File(mFilePath));
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,bitmapUri);
+                startActivityForResult(intent,REQ_2);
+            }
+          public void customCamera(View view){
+         }
+        //打开图库
+        public void Album(View view){
+            //选择界面的intent
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            //设置内容为image类型
+            intent.setType("image/*");
+            //打开图库界面
+            startActivityForResult(intent,REQ_3);
+        }
+    
+        
+    
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //结果码
+        if(resultCode==RESULT_OK){
+            Log.d("TakePhoto","result_ok");
+            if(data==null){
+                return;
+            }
+            //压缩图，请求码
+            if(requestCode==REQ_1){
+                Bundle bundle = data.getExtras();
+                bitmap = (Bitmap) bundle.get("data");
+                imageView.setImageBitmap(bitmap);
+            }
+            //原图，请求码,这边必须要有Camera权限，要不然呢fis会报null
+            else if(requestCode==REQ_2){
+                try {
+                    fis=new FileInputStream(mFilePath);
+                    bitmap=BitmapFactory.decodeStream(fis);
+                    imageView.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } finally{
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }else if(requestCode==REQ_3){
+                Uri uri = data.getData();
+                
+            }
+        }
+    }
+      
+    private void destroyBitmap() {
+        if(bitmap!=null&&!bitmap.isRecycled()){
+            bitmap.recycle();
+            bitmap = null;
+        }
+    }
+}
